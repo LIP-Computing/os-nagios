ed3ebda2976fc03499f47fa82812037c1c450f25
mariojmdavid <mariojmdavid@gmail.com>
2016-06-06 08:24:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Openstack Nagios plugins
+========================
+
+Nagios plugins for openstack using the shade library
---
+++ plugins/service-glance.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import shade
+
+STATUS_OK = 0
+STATUS_WARNING = 1
+STATUS_CRITICAL = 2
+STATUS_UNKNOWN = 3
